c15377ff231fdb600ac6c51199d8c4f9e68a3c38
Arsen <52761327+cly4auHo@users.noreply.github.com>
2020-02-16 03:24:54 +0200

    Update and rename CallPrivateMethod.java to Reflect/CallPrivateMethod.java

--- Reflect/CallPrivateMethod.java
+++ Reflect/CallPrivateMethod.java
@@ -5,10 +5,10 @@ import java.lang.reflect.Method;
 
 public class CallPrivateMethod {
     public static void main(String[] args) throws NoSuchMethodException, InvocationTargetException, IllegalAccessException {
-        Refector refector = new Refector();
+        Refector refector = new Refector(); 
         Class clazz = Refector.class;
 
-        Method method = clazz.getDeclaredMethod("methodPrivate", java.lang.String.class, int.class);
+        Method method = clazz.getDeclaredMethod("methodPrivateName", java.lang.String.class, int.class);
         method.setAccessible(true);
         method.invoke(refector, "Lol", 15);
     }
